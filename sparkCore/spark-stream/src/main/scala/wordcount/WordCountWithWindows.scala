48336566f75084f6ba56b7def290b073d129cf98
Administrator <284477367@qq.com>
2019-02-21 08:47:30 +0800

    afddsafdsaf

---
+++ sparkCore/spark-stream/src/main/scala/wordcount/WordCountWithWindows.scala
@@ -0,0 +1,27 @@
+package wordcount
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object WordCountWithState {
+
+
+  def main(args: Array[String]): Unit = {
+    val sc = new SparkConf().setAppName("WordCountWithState").setMaster("local[*]")
+    val ssc = new StreamingContext(sc, Seconds(5))
+    ssc.sparkContext.setCheckpointDir("C:\\Users\\Administrator\\Desktop\\checkPoint")
+    val sts = ssc.socketTextStream("hadoop102",9999)
+    val wordRes = sts.flatMap(_.split(" ")).map((_,1))
+//    wordRes.print()
+//    println("+++++++++++++++++++++++++++++++++++++++++")
+    val wordStateRes=wordRes.updateStateByKey[Int]((word:Seq[Int],count:Option[Int])=>{
+//      println("sum is "+word.mkString(","))
+//      println("word is "+count)
+      Some(word.sum+count.getOrElse(0))
+    })
+    wordStateRes.print()
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+}
